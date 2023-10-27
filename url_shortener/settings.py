43c278dfe835e5d175d0fa538ef13860f9c431f3
Varun-Kolanu <kolanuvarun739@gmail.com>
2023-10-28 01:11:37 +0530

    Configured setting.py for production build static

--- url_shortener/settings.py
+++ url_shortener/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -53,6 +54,9 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'url_shortener.urls'
+
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 TEMPLATES = [
     {
